9623efdf877211c43ec504e0a943ea643dd23a34
abhipoo <abhishekpoojary16@gmail.com>
2017-04-09 20:39:38 +0530

    Added Code Files

---
+++ UpTime/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // Uptime Core Code Begins
+
+        public static TimeSpan GetUpTime()
+        {
+            return TimeSpan.FromMilliseconds(GetTickCount64());
+        }
+
+        [DllImport("kernel32")]
+        extern static UInt64 GetTickCount64();
+
+        // Uptime Core Code Ends
+
+
+        //Action on MouseOver
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            TimeSpan ts = GetUpTime();
+
+            string TimeString = Convert.ToString(ts);
+
+            TimeString = TimeString.Remove(TimeString.IndexOf('.'));
+
+            notifyIcon1.Text = "UpTime : " + TimeString;
+        }
+
+        // Action on Clicking 'End'
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ UpTime/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UpTime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ingram Micro")]
+[assembly: AssemblyProduct("UpTime")]
+[assembly: AssemblyCopyright("Copyright © Ingram Micro 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4b51193-d699-4035-8135-34a42f1913fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
